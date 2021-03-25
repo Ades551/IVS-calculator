d792c8addb5f2ff9289553ccbbda82848866c32a
Adam Rajko <xrajko00@stud.fit.vutbr.cz>
2021-03-25 15:25:23 +0100

    Added: empty functions

--- src/lib/cmath.cpp
+++ src/lib/cmath.cpp
@@ -1,5 +1,33 @@
 #include "cmath.h"
 
-double CalcMath::add(double number1, double number2){
-    return number1 + number2;
+double CalcMath::add(double number_1, double number_2){
+    return false;
+}
+
+double CalcMath::sub(double number_1, double number_2){
+    return false;
+}
+
+double CalcMath::mult(double number_1, double number_2){
+    return false;
+}
+
+double CalcMath::div(double dividend, double divisor){
+    return false;
+}
+
+double CalcMath::pow(double basis, double exponent){
+    return false;
+}
+
+double CalcMath::root(double n_root, double number){
+    return false;
+}
+
+double CalcMath::log(double basis, double logarithm){
+    return false;
+}
+
+unsigned long CalcMath::fact(unsigned long number){
+    return false;
 }
