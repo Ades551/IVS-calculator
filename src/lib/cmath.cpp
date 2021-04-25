04c023280612f701674da232e1dbf5f8dd8552a8
RichardBuban <sisoriso777@gmail.com>
2021-04-25 02:13:44 +0200

    Mathlib - added comment and edited error handling

--- src/lib/cmath.cpp
+++ src/lib/cmath.cpp
@@ -1,6 +1,6 @@
 #include "cmath.h"
 #include <math.h>
-#include "stdio.h"
+#include <stdexcept>
 
 double CalcMath::add(double number_1, double number_2){
     return number_1 + number_2;
@@ -16,7 +16,7 @@ double CalcMath::mult(double number_1, double number_2){
 
 double CalcMath::div(double dividend, double divisor){
 	if(divisor == 0)
-		return false;
+		throw std::invalid_argument("Division by zero!");
     return dividend / divisor;
 }
 
@@ -24,26 +24,32 @@ double CalcMath::pow(double basis, double exponent){
     return std::pow(basis,exponent);
 }
 
-double CalcMath::root(double n_root, double number){
-	if(n_root != lround(n_root) || n_root < 1)
-		return false;
-	long ln_root = lround(n_root);
+double CalcMath::root(double degree, double number){
+	if(degree != lround(degree))
+		throw std::invalid_argument("nth root, n is not an integer!");
+	if(degree < 1)
+		throw std::invalid_argument("nth root, n is lower than 1!");
+	long ln_root = lround(degree);
 	if(number < 0 && (ln_root % 2) == 0)
-		return false;
+		throw std::invalid_argument("nth root, number cannot be negative if root is even!");
 	if(number < 0)
-		return -std::pow(-number,1/n_root);
-	return std::pow(number,1/n_root);
+		return -std::pow(-number,1/degree);
+	return std::pow(number,1/degree);
 }
 
-double CalcMath::log(double basis, double logarithm){
-	if(basis <= 0 || basis == 1 || logarithm <= 0)
-		return false;
-	return std::log(logarithm)/std::log(basis);
+double CalcMath::log(double basis, double number){
+	if(basis <= 0)
+		throw std::invalid_argument("basis cannot be lower or equal to zero!");
+	if(basis == 1)
+		throw std::invalid_argument("basis cannot be 1!");
+	if(number <= 0)
+		throw std::invalid_argument("number cannot be lower or equal to zero!");
+	return std::log(number)/std::log(basis);
 }
 
 unsigned long CalcMath::fact(double number){
 	if(number < 0 || number != lround(number))
-    	return false;
+		throw std::invalid_argument("number must be positive integer!");
 	unsigned long fact = 1;
 	for(unsigned long i = number; i > 1;i--)
 		fact *= i;
--- src/lib/cmath.h
+++ src/lib/cmath.h
@@ -4,6 +4,7 @@ public:
      * @brief Math addition
      * @param number_1 first number for addition
      * @param number_2 second number for addition
+     * @return Sum of number_1 and number_2
      */
     static double add(double number_1, double number_2);
 
@@ -11,6 +12,7 @@ public:
      * @brief Math subtraction
      * @param number_1 first number for subtraction
      * @param number_2 second number for subtraction
+     * @return Substraction of number_1 and number_2
      */
     static double sub(double number_1, double number_2);
 
@@ -18,6 +20,7 @@ public:
      * @brief Math multiplication
      * @param number_1 first number for multiplication
      * @param number_2 second number for multiplication
+     * @return Multiplication of number_1 and number_2
      */
     static double mult(double number_1, double number_2);
 
@@ -25,6 +28,8 @@ public:
      * @brief Math division
      * @param dividend dividend
      * @param divisor divisor
+     * @return Divident divided by divisor
+     * @throw invalid_argument Division by zero!
      */
     static double div(double dividend, double divisor);
 
@@ -32,26 +37,37 @@ public:
      * @brief Math exponentiation
      * @param basis base for exponent
      * @param exponent exponent
+     * @return Basis to the power of exponent
      */
     static double pow(double basis, double exponent);
 
     /**
      * @brief Math square root
-     * @param n_root n-th root
-     * @param number Neviem prosim doplnit xD
+     * @param degree degree of the root
+     * @param number number from which n-th root is calculated
+     * @return N-th root of number
+     * @throw invalid_argument nth root, n is not an integer!
+     * @throw invalid_argument nth root, n is lower than 1!
+     * @throw invalid_argument nth root, number cannot be negative if root is even!
      */
-    static double root(double n_root, double number);
+    static double root(double degree, double number);
 
     /**
      * @brief Math logarithm
      * @param basis base of logarithm
      * @param logarithm logarithm
+     * @return Logarithm from number with base basis
+     * @throw invalid_argument basis cannot be lower or equal to zero!
+     * @throw invalid_argument basis cannot be 1!
+     * @throw invalid_argument number cannot be lower or equal to zero!
      */
-    static double log(double basis, double logarithm);
+    static double log(double basis, double number);
 
     /**
      * @brief Math factorial
-     * @param number factorial number 
+     * @param number factorial number
+     * @retuern Factorial of number
+     * @throw invalid_argument number must be positive integer!
      */
     static unsigned long fact(double number);
 };
